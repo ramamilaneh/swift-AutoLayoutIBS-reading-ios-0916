0e8e4460bedd72c50276d83dae8b196b1932d277
Rama Milaneh <ramamilaneh@Ramas-MacBook-Air.local>
2016-10-12 14:39:50 -0400

    Done

--- ViewFun/ViewController.swift
+++ ViewFun/ViewController.swift
@@ -9,17 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var purple: UIView!
+    
+    @IBOutlet weak var pink: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let blueView = UIView(frame: CGRect(x: 20.0, y: 20.0, width: 100.0, height: 100.0))
-        blueView.backgroundColor = UIColor.blue
-        view.addSubview(blueView)
-        
-        let redView = UIView(frame: CGRect(x: 150.0, y: 150.0, width: 40.0, height: 40.0))
-        redView.backgroundColor = UIColor.red
-        view.addSubview(redView)
-    }
+            
+           }
     
 }
